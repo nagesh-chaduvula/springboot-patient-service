16c550dfcce36efa61da2ca8b4b74e30b1f5e5e7
Nageswarareddy Chaduvula <dt241115@ssnc-corp.global>
2025-04-13 13:56:12 +0530

    Initial commit

---
+++ src/main/java/com/springboot/patient/SpringbootPatientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPatientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPatientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/patient/SpringbootPatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
